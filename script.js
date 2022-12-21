3dfd03d5414977b1a9d883912df9f838be0fa909
René Delion <rene.leon.delion@gmail.com>
2022-12-21 02:17:03 +0100

    Add getPlayerChoice function

--- script.js
+++ script.js
@@ -4,7 +4,49 @@ const Choice = {
     SCISSORS: "Scissors"
 };
 
+const Gamestate = {
+    WON: "You have won!",
+    LOST: "You have lost!",
+    DRAW: "It`s a draw!"
+};
+
+/**
+ * @param {String*} word 
+ * @returns a the word capitalized
+ */
+function capitalize(word) {
+    arr = Array.from(word);
+    arr[0] = arr[0].toUpperCase(); 
+    for(let i = 1; i < arr.length; i++) {
+        arr[i] = arr[i].toLowerCase();
+    }
+    return arr.join("");
+}
+
+/**
+ * @returns Returns a random pick from Rock, Paper, Scissors
+ */
 function getComputerChoice() {
     const CHOICES = [Choice.ROCK, Choice.PAPER, Choice.SCISSORS];
     return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
+/**
+ * @param {String*} choice 
+ * @returns the players pick from Rock, Paper, Scissors. If players choice is not reasonable returns null
+ */
+function getPlayerChoice(choice) {
+    choice = capitalize(choice);
+    if(choice == "Rock") {
+        return Choice.ROCK;
+    }
+    else if(choice == "Paper") {
+        return Choice.PAPER;
+    }
+    else if(choice == "Scissor") {
+        return Choice.SCISSORS;
+    }
+    else {
+        return null;
+    }
 }
